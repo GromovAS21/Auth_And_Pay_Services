f5570418833b30438a2d2f2a918b48695541542b
Anton Gromov <seed14112@yandex.ru>
2025-05-06 11:32:38 +0300

    [+] Расписывание схем

--- schemas.py
+++ schemas.py
@@ -1,23 +1,42 @@
-from pydantic import BaseModel
+from typing import List
+
+from pydantic import BaseModel, EmailStr, Field
 
 
 class CreateUser(BaseModel):
     """Схема для создания пользователя."""
 
-    email: str
-    username: str
-    first_name: str
-    last_name: str
-    password: str
+    email: EmailStr = Field(..., description="Email пользователя")
+    username: str = Field(..., min_length=2, max_length=50, description="Логин пользователя")
+    first_name: str = Field(..., min_length=2, max_length=50, description="Имя пользователя")
+    last_name: str = Field(..., min_length=2, max_length=50, description="Фамилия пользователя")
+    password: str = Field(..., min_length=8, description="Пароль")
 
 
-class UpdateUser(BaseModel):
+class UpdateUser(CreateUser):
     """Схема для обновления пользователя."""
 
-    email: str
-    username: str
-    first_name: str
-    last_name: str
-    password: str
-    is_active: bool
-    is_admin: bool
+    password: None = Field(None, exclude=True)
+    is_active: bool = Field(default=True, description="Активный статус пользователя")
+    is_admin: bool = Field(default=False, description="Админ статус пользователя")
+
+
+class User(BaseModel):
+    """Схема для получения пользователя."""
+
+    id: int = Field(..., description="ID пользователя")
+    email: EmailStr = Field(..., description="Email пользователя")
+    full_name: str = Field(..., description="ФИО пользователя")
+
+class Account(BaseModel):
+    """Схема для получения аккаунта пользователя."""
+    id: int = Field(..., description="ID аккаунта")
+    total: float = Field(..., description="Баланс пользователя")
+
+
+class UsersWithAccounts(BaseModel):
+    """Схема для получения пользователей с аккаунтами."""
+    id: int = Field(..., description="ID пользователя")
+    email: EmailStr = Field(..., description="Email пользователя")
+    full_name: str = Field(..., description="ФИО пользователя")
+    accounts: List[Account] = Field(..., description="Список аккаунтов пользователя")
